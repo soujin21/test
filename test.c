1237176b66551bda2ac5a208da2121e4e58fd72c
soujin21 <taingsoujin21@gmail.com>
2025-08-05 11:51:44 +0700

    first commit

---
+++ test.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+	 int code;
+	 char name[20];
+	 int qty;
+	 double price;
+	 int discount;
+	 double total;
+	 double payment;
+	printf("code: ");
+	scanf("%d",&code);
+	printf("name: ");
+	scanf("%s",&name);
+	printf("qty: ");
+	scanf("%d",&qty);
+	printf("price: ");
+	scanf("%lf",&price);
+	printf("discount: ");
+	scanf("%d",&discount);
+	total= price*qty;
+	payment= total-(total*discount)/100;
+	printf("%lf\n",total);
+	printf("%lf",payment);
+	
+	
+}
